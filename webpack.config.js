9e23d1b5053f084a880ae32d550d14282d0dd142
Ruslan Krokhin <ulitcos@gmail.com>
2020-11-08 23:28:37 +0300

    fixed library name

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
-    library: 'sum',
+    library: 'objectGrep',
     libraryTarget: 'umd',
   }
 };
